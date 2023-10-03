f1e68e304299cc6846df47e226d8c35cda920149
Korolev E <nuuchcha@bk.ru>
2023-10-03 13:20:59 +0500

    add resources

--- src/test/java/ru/netology/services/RestDaysServiceTest.java
+++ src/test/java/ru/netology/services/RestDaysServiceTest.java
@@ -1,18 +1,21 @@
 package ru.netology.services;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestDaysServiceTest {
 
-    @Test
-    void countDaysRest() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/restdays.csv")
+    void countDaysRest(int income, int expenses, int threshold, int expected) {
         RestDaysService service = new RestDaysService();
 
         // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+        // int income = 10_000;
+        // int expenses = 3_000;
+        // int threshold = 20_000;
+        // int expected = 3;
 
         // вызываем целевой метод:
         int count = service.calculate(income, expenses, threshold);
@@ -21,7 +24,7 @@ public class RestDaysServiceTest {
         Assertions.assertEquals(expected, count);
     }
 
-    @Test
+    /*   @Test
     void countDaysRest1() {
         RestDaysService service = new RestDaysService();
 
@@ -37,6 +40,6 @@ public class RestDaysServiceTest {
         System.out.println("В году " + count + " месяц(а,ев) отдыха");
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, count);
-    }
+    } */
 
 }
